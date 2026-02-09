2d9ba03209f217dc1f7cf83b00e293e14720f83c
greenpop86 <Smcintosh@worcester.edu>
2026-02-09 18:33:39 -0500

    Create MusicVizApp.swift file with SwiftUI app entry point and dark theme support

---
+++ MusicVizApp.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+@main
+struct MusicVizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .preferredColorScheme(.dark) // Set app to use dark theme
+        }
+    }
+}
